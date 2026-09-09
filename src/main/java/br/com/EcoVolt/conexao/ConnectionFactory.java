3b57c7a6c26cd87ece3c8ae623ab74e06f3a9f98
matheusluca <luca_matheus@live.com>
2026-09-08 22:05:41 -0300

    Feat: Criacao da classe responsavel por retornar a conexao.

--- src/main/java/br/com/EcoVolt/conexao/ConnectionFactory.java
+++ src/main/java/br/com/EcoVolt/conexao/ConnectionFactory.java
@@ -1,6 +1,14 @@
 package br.com.EcoVolt.conexao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
 
-
+    public static Connection getconnection() throws ClassNotFoundException, SQLException {
+        Class.forName("oracle.jdbc.OracleDriver");
+            Connection conexao = DriverManager.getConnection("dbc:oracle:thin:@oracle.fiap.com.br:1521:orcl", "rm572228", "260497");
+            return conexao;
+    }
 }
